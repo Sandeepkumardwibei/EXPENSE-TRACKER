4324a0ef11dd95d876875187a7268af14741fae3
Sandeepkumardwibei <sujatadwibei700@gmail.com>
2025-02-21 21:46:46 +0530

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,9 @@
-import svgToDataUri from "mini-svg-data-uri";
+
 import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 
-/** @type {import('tailwindcss').Config} */
+ {import('tailwindcss').Config} 
+ const svgToDataUri = require("mini-svg-data-uri");
+
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	darkMode: "class",
