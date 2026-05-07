4631bf475ec2227e134dcea372511ca7c8cea08b
Sandeepkumardwibei <Sandeepkumardwibei@users.noreply.github.com>
2026-05-08 00:25:18 +0530

    Fixed blank page bug caused by Apollo Client URI and undefined states

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,7 @@ import GridBackground from "./components/ui/GridBackgroun.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-	// TODO => Update the uri on production
-	uri: 'http://localhost:4000/graphql',
+	uri: import.meta.env.MODE === "development" ? "http://localhost:4000/graphql" : "/graphql",
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
 	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
